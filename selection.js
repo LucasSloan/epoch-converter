f0ac5413c4cd3433e7ae78e1d986ea34b7bb9407
PotatoDumplings <lucas@streak.com>
2016-01-28 19:29:09 -0800

    Working version of epoch converter.

--- selection.js
+++ selection.js
@@ -1,68 +1,91 @@
 $(document).ready(function() {
-    $('body').append("<div id=\"ec-bubble\"><div id=\"ec-bubble-text\"></div><div id=\"ec-bubble-close\"></div></div>");
-    
-    $('#ec-bubble-close').click(function() {
-        hideBubble();
-    });
-    
-	$(document).dblclick(function(e) {
-		processSelection(e);
-	});
-	
-	$(document).bind('mouseup', function(e) {
-		processSelection(e);
-	});
+    $(document).bind('mousedown', function() {
+        $(document).one('mouseup', function(event) {
+            var selectedText = getSelectedText();
 
+            if (!isNaN(parseInt(selectedText))) {
+                var textAsInteger = parseInt(selectedText);
+                if (isValidTimestamp(textAsInteger)) {
+                    var timestampType = getTimestampType(textAsInteger);
+                    var localDateTime = getTimestampAsLocalDateTime(textAsInteger);
+
+                    // Show the tooltip
+                    $('<div/>').qtip({
+                        content: {
+                            title: timestampType,
+                            text: localDateTime,
+                            button: true
+                        },
+                        position: {
+                            target: 'mouse',
+                            adjust: { mouse: false }
+                        },
+                        show: false,
+                    }).qtip('api').show(event);
+                }
+            }
+        });
+    });
 });
 
-function processSelection(e) {	
-    var text = getSelectedText();
+var formatString = "M/D/YYYY, H:mm:ss A [GMT]Z";
+var yearTwoThousand = moment.utc({ year :2000, month :0, day :0});
+var yearTwentyFifty = moment.utc({ year :2050, month :0, day :0});
 
-    if ($.isNumeric(text)) {
-		var humanReadableDate = convertTimestamp(text);
-        showBubble(e, humanReadableDate);        
-	}
+function getTimestampAsLocalDateTime(ts) {
+	var date = moment(ts * 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return date.format(formatString);
+    }
+    date = moment(ts);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return date.format(formatString);
+    }
+    date = moment(ts / 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return date.format(formatString);
+    }
+}
+
+function getTimestampType(ts) {
+	var date = moment(ts * 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return "Assuming that this timestamp is in seconds:";
+    }
+    date = moment(ts);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return "Assuming that this timestamp is in milliseconds:";
+    }
+    date = moment(ts / 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return "Assuming that this timestamp is in nanoseconds:";
+    }
+}
+
+function isValidTimestamp(ts) {
+	var date = moment(ts * 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return true;
+    }
+    date = moment(ts);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return true;
+    }
+    date = moment(ts / 1000);
+    if (date.isAfter(yearTwoThousand) && date.isBefore(yearTwentyFifty)) {
+        return true;
+    }
+    return false;
 }
 
 function getSelectedText() {
 	var text = "";
-	
+
     if (window.getSelection) {
         text = window.getSelection().toString();
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
-	
+
 	return text;
 }
-
-function convertTimestamp(ts) {
-	var date = new Date(ts * 1000);
-	var dateStr = "";
-    
-    var d = date.getDate();
-    var m = date.getMonth()+1;
-    var y = date.getFullYear();
-    dateStr += (m<=9?'0'+m:m) + "/" + (d<=9?'0'+d:d) + "/" + y + " - ";
-  
-    var h = date.getHours();
-    var mi = date.getMinutes();
-    var s = date.getSeconds(); 
-    dateStr += (h<=9?'0'+h:h) + ":" + (mi<=9?'0'+mi:mi) + ":" + (s<=9?'0'+s:s);
-    
-	return dateStr;
-}
-
-function showBubble(e, text) {
-    $('#ec-bubble').css('top', e.pageY + 20 + "px");
-    $('#ec-bubble').css('left', e.pageX - 85 + "px");
-    $('#ec-bubble-text').html(text);
-    $('#ec-bubble').css('visibility', 'visible');
-}
-
-function hideBubble() {
-    $('#ec-bubble').css('visibility', 'hidden');
-    $('#ec-bubble-text').html("");
-}
-
-
